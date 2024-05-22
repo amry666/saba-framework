df65430cc83367f940ab19685b89121978b6fd8f
amry666 <amry.maftuh85@gmail.com>
2024-05-22 16:07:20 +0000

    20240522-1531

---
+++ static/js/129.be5593c8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsaba-react-framework"]=this["webpackJsonpsaba-react-framework"]||[]).push([[129],{1273:function(t,a,c){},794:function(t,a,c){"use strict";c.r(a);var n=c(33),r=c(5),e=(c(1273),c(81)),o=c(0);a.default=function(){var t,a=Object(n.f)(),c=Object(e.a)().t;return(null===(t=window.history)||void 0===t?void 0:t.length)>1?Object(o.jsx)(r.i,{color:"primary",className:"btn-sm-block mb-1 me-1",onClick:function(){a(-2)},children:c("Back")}):null}}}]);
+//# sourceMappingURL=129.be5593c8.chunk.js.map
